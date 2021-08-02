3586f5925bb4d5d1999f305813c0223a663fbee1
LHWOOD <hutsonwood@gmail.com>
2021-08-02 14:04:38 -0600

    created server connection

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const sequelize = require("sequelize");
+const routes = require("./controllers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`LHWOOD, now listening on ${PORT}`));
+});
