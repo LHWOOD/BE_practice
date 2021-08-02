6742e105d1e073df452e08b22f4f070a6877b101
LHWOOD <hutsonwood@gmail.com>
2021-08-02 15:37:23 -0600

    seeded db and tested routes via insomnia

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    usename: {
+    username: {
       type: DataTypes.STRING,
     },
     email: {
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,13 @@
 const sequelize = require("../config/connection");
-const User = require("../models");
+const { User } = require("../models");
 
 const userSeedData = require("./userSeedData.json");
 
 const seedDataBase = async () => {
   await sequelize.sync({ force: true });
-
-  await User.bulkCreate(userSeedData);
+  console.log("\n----- DATABASE SYNCED -----\n");
+  const user = await User.bulkCreate(userSeedData);
+  console.log("\n----- DATABASE SEEDED -----\n");
 
   process.exit(0);
 };
