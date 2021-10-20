87439c36936b7f0def58a589f11741b9c6bb6819
LHWOOD <hutsonwood@gmail.com>
2021-10-20 12:25:04 -0600

    changed dep

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require("express");
 const sequelize = require("./config/connection");
 const routes = require("./controllers");
 
-const stuff = require("test");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
